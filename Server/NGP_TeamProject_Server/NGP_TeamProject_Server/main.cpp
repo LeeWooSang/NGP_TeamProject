8df1b5e85512068874bc608125e9006982a8310a
Sa Myoung Jin <ekdskrnl105@gmail.com>
2018-11-01 16:59:28 +0900

    no message

--- Server/NGP_TeamProject_Server/NGP_TeamProject_Server/main.cpp
+++ Server/NGP_TeamProject_Server/NGP_TeamProject_Server/main.cpp
@@ -1,14 +1,17 @@
 #include "Defines.h"
 #include "ServerFramework.h"
 
+
 int main()
 {
 	CServerFramework* Server = nullptr;
 	Server = new CServerFramework;
 	if (Server == nullptr)
 		return 0;
-	Server->AcceptClient();
-	
+
+	//DWORD WINAPI ServerThread()
+	Server->ServerThread();
+
 	delete Server;
 	Server = nullptr;
 }
