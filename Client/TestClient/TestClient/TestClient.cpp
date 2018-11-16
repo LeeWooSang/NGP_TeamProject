2349df0be5cf2df8e5ac965a6790eb8de2c5568e
이우상 <lus0506@gmail.com>
2018-11-16 16:55:20 +0900

    테스트 클라이언트 플레이어 정보 출력하기

--- Client/TestClient/TestClient/TestClient.cpp
+++ Client/TestClient/TestClient/TestClient.cpp
@@ -1,6 +1,8 @@
 #include "Defines.h"
 #include "TestClient.h"
 
+SC_RUN sc_runPacket;
+byte playerInfo = 0;
 
 int main()
 {
@@ -12,15 +14,15 @@ int main()
 		return 0;
 
 	BOOL optval = true;
-	
+
 	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock == INVALID_SOCKET)
 	{
 		err_quit("socket( )");
 		return 0;
 	}
-		
-	
+
+
 	//connect
 	SOCKADDR_IN serveraddr;
 	ZeroMemory(&serveraddr, sizeof(serveraddr));
@@ -38,21 +40,21 @@ int main()
 	// 변수 초기화
 	SC_INIT sc_initPacket;
 	memset(&sc_initPacket, 0, sizeof(sc_initPacket));
-	
 	CS_RUN cs_runPacket;
-	SC_RUN sc_runPacket;
+
 	//cs_runPacket,sc_runPacket 0으로 초기화
 	memset(&cs_runPacket, 0, sizeof(cs_runPacket));
 	memset(&sc_runPacket, 0, sizeof(sc_runPacket));
 	size_t packetSize = 0;
-	byte playerInfo = 0;							//플레이어 자신의 정보
+
+	playerInfo = 0;	//플레이어 자신의 정보
 
 	byte gameState = TYPE_INIT;
 	while (true)
 	{
 		switch (gameState)
 		{
-		// Init 상태
+			// Init 상태
 		case TYPE_INIT:
 			// 고정길이 받기
 			packetSize = sizeof(SC_INIT);
@@ -92,80 +94,56 @@ int main()
 */
 			break;
 
-		//Run 상태
+			//Run 상태
 		case TYPE_RUN:
-			while (true)
+			packetSize = sizeof(SC_RUN);
+			//cout << "초기 위치 서버에서 받아야 합니다" << endl;
+			// 서버에서 초기 플레이어 위치 받기
+
+			retval = recvn(sock, (char*)&packetSize, sizeof(packetSize), 0);
+			if (retval == SOCKET_ERROR)
 			{
-				packetSize = sizeof(SC_RUN);
-				//cout << "초기 위치 서버에서 받아야 합니다" << endl;
-				// 서버에서 초기 플레이어 위치 받기
-				
-				retval = recvn(sock, (char*)&packetSize, sizeof(packetSize), 0);
-				if (retval == SOCKET_ERROR)
+				err_display("recv()");
+				break;
+			}
+			//cout << "초기위치 받기를 기다리는중(고정 크기)" << endl;
+			// 가변 길이
+			retval = recvn(sock, (char*)&sc_runPacket, sizeof(sc_runPacket), 0);
+			if (retval == SOCKET_ERROR)
+			{
+				err_display("recv()");
+				break;
+			}
+
+			//cs_runPacket에 player정보는 sc_initPacket에서 서버에서 받은 플레이어 번호를 그대로 대입
+			cs_runPacket.player = sc_initPacket.player;
+
+			while (Input_Keyboard(cs_runPacket))
+			{
+				//키 입력이 있을경우에만 전송하게 구현하기위해 
+				if (cs_runPacket.key != KEY_IDLE)
 				{
-					err_display("recv()");
-					break;
-				}
-				//cout << "초기위치 받기를 기다리는중(고정 크기)" << endl;
-				// 가변 길이
-				retval = recvn(sock, (char*)&sc_runPacket, sizeof(sc_runPacket), 0);
-				if (retval == SOCKET_ERROR)
-				{
-					err_display("recv()");
-					break;
-				}
-				
-		/*		cout << "초기위치 받기를 기다리는중(가변 길이)" << endl;
-				cout <<"sc_runPacket[0]" <<sc_runPacket.pos[0].X<<","<<sc_runPacket.pos[0].Y<<endl;
-				cout << "sc_runPacket[1]" << sc_runPacket.pos[1].X << "," << sc_runPacket.pos[1].Y << endl;
-*/
-
-				if (playerInfo == PLAYER_1) {
-					cout << "플레이어 위치 정보 x: " << sc_runPacket.pos[PLAYER_1].X<< ",";
-					cout << "플레이어 위치 정보 y: " << sc_runPacket.pos[PLAYER_1].Y<< endl;
-
-					cout << "상대방 위치 정보 x: " << sc_runPacket.pos[PLAYER_2].X << ",";
-					cout << "상대방 위치 정보 y: " << sc_runPacket.pos[PLAYER_2].Y << endl;
-				}
-				else if (playerInfo == PLAYER_2)
-				{
-					cout << "플레이어 위치 정보 x: " << sc_runPacket.pos[PLAYER_2].X << ",";
-					cout << "플레이어 위치 정보 y: " << sc_runPacket.pos[PLAYER_2].Y << endl;
-
-					cout << "상대방 위치 정보 x: " << sc_runPacket.pos[PLAYER_1].X << ",";
-					cout << "상대방 위치 정보 y: " << sc_runPacket.pos[PLAYER_1].Y << endl;
-				}
-				//cs_runPacket에 player정보는 sc_initPacket에서 서버에서 받은 플레이어 번호를 그대로 대입
-				cs_runPacket.player = sc_initPacket.player;
-
-				while (true)
-				{
-					Input_Keyboard(cs_runPacket);
-
-					//키 입력이 있을경우에만 전송하게 구현하기위해 
-					if (cs_runPacket.key != KEY_IDLE)
+					// 고정길이 : 패킷 크기 전송
+					packetSize = sizeof(cs_runPacket);
+					retval = send(sock, (char*)&packetSize, sizeof(packetSize), 0);
+					if (retval == SOCKET_ERROR)
 					{
-						// 고정길이 : 패킷 크기 전송
-						packetSize = sizeof(cs_runPacket);
-						retval = send(sock, (char*)&packetSize, sizeof(packetSize), 0);
-						if (retval == SOCKET_ERROR)
-						{
-							err_display("send( )");
-							return 0;
-						}
-						//cout << "패킷 크기(고정길이) 전송 - " << retval << "Byte" << endl;
-
-						// 가변길이 : 실제 패킷 전송
-						retval = send(sock, (char*)&cs_runPacket, sizeof(cs_runPacket), 0);
-						if (retval == SOCKET_ERROR)
-						{
-							err_display("send( )");
-							return 0;
-						}
-						Release_Key(cs_runPacket);
+						err_display("send( )");
+						return 0;
 					}
+					//cout << "패킷 크기(고정길이) 전송 - " << retval << "Byte" << endl;
+
+					// 가변길이 : 실제 패킷 전송
+					retval = send(sock, (char*)&cs_runPacket, sizeof(cs_runPacket), 0);
+					if (retval == SOCKET_ERROR)
+					{
+						err_display("send( )");
+						return 0;
+					}
+					Release_Key(cs_runPacket);
 				}
 			}
+
 			break;
 		}
 
@@ -222,7 +200,7 @@ int recvn(SOCKET s, char* buf, int len, int flags)
 
 //void Print_Position()
 
-void Input_Keyboard(CS_RUN& runPacket)
+bool Input_Keyboard(CS_RUN& runPacket)
 {
 	runPacket.type = TYPE_RUN;
 	//runPacket.player = PLAYER_1;
@@ -235,22 +213,28 @@ void Input_Keyboard(CS_RUN& runPacket)
 		switch (key)
 		{
 		case RIGHT:
+			show_Position();
 			runPacket.key = KEY_RIGHT;
 			cout << "RIGHT 방향키 눌림" << endl;
 			break;
 		case LEFT:
+			show_Position();
 			runPacket.key = KEY_LEFT;
 			cout << "LEFT 방향키 눌림" << endl;
 			break;
 		case UP:
+			show_Position();
 			runPacket.key = KEY_UP;
 			cout << "UP 방향키 눌림" << endl;
 			break;
 		case SPACE:
+			show_Position();
 			runPacket.key = KEY_SPACE;
 			cout << "SPACE 방향키 눌림" << endl;
 			break;
 		}
+
+		return true;
 		//cout << myInfo.name << " - X : " << myInfo.pos.X << ", Y : " << myInfo.pos.Y << endl;
 	}
 	//else
@@ -259,7 +243,7 @@ void Input_Keyboard(CS_RUN& runPacket)
 	//	//cout << "키보드 안 눌림" << endl;
 	//	break;
 	//}
-	
+	return false;
 }
 
 //Send 한 Key값 해제
@@ -272,4 +256,26 @@ void Release_Key(CS_RUN& runPacket)
 			runPacket.key = KEY_IDLE;
 		}
 	}
+}
+
+void show_Position()
+{
+	cout << "=================================================" << endl;
+	if (playerInfo == PLAYER_1) 
+	{
+		cout << "플레이어 위치 정보 x: " << sc_runPacket.pos[PLAYER_1].X << ",";
+		cout << "플레이어 위치 정보 y: " << sc_runPacket.pos[PLAYER_1].Y << endl;
+
+		cout << "상대방 위치 정보 x: " << sc_runPacket.pos[PLAYER_2].X << ",";
+		cout << "상대방 위치 정보 y: " << sc_runPacket.pos[PLAYER_2].Y << endl;
+	}
+	else if (playerInfo == PLAYER_2)
+	{
+		cout << "플레이어 위치 정보 x: " << sc_runPacket.pos[PLAYER_2].X << ",";
+		cout << "플레이어 위치 정보 y: " << sc_runPacket.pos[PLAYER_2].Y << endl;
+
+		cout << "상대방 위치 정보 x: " << sc_runPacket.pos[PLAYER_1].X << ",";
+		cout << "상대방 위치 정보 y: " << sc_runPacket.pos[PLAYER_1].Y << endl;
+	}
+	cout << "=================================================" << endl;
 }
