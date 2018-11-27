fbb497e6b860e1e573406698347954e65a59c4fb
Sa Myoung Jin <ekdskrnl105@gmail.com>
2018-11-27 15:58:14 +0900

    skillManager,collision 커밋

---
+++ Server/NGP_TeamProject_Server/NGP_TeamProject_Server/SkillManager.cpp
@@ -0,0 +1,91 @@
+#include "SkillManager.h"
+
+
+
+SkillManager::SkillManager()
+{
+
+}
+
+SkillManager::~SkillManager()
+{}
+
+
+void SkillManager::addSkill(byte& player,COORD& skillPos)
+{
+	
+	SC_SKILL fireballInfo;
+	fireballInfo.type = TYPE_SKILL;
+	fireballInfo.player = player;
+	if (player == PLAYER_1)
+	{
+		fireballInfo.isRight = true;
+	}
+	else {
+		fireballInfo.isRight = false;
+	}
+	fireballInfo.skillPos = skillPos;
+	fireballInfo.isCrush = false;
+	fireballInfo.skillIndex = skillVector.size();
+
+	//cout << "현재 스킬 발생 개수" <<skillVector.size()<< endl;
+	skillVector.emplace_back(fireballInfo);
+
+
+}
+
+
+
+void SkillManager::update(float fTimeElapsed,COORD& player1,COORD& player2)
+{
+	if (skillVector.size() >= 1) {
+		checkCollision(player1, player2); //업데이트 전에 충돌검사를 미리 실시
+		for (iter = skillVector.begin(); iter != skillVector.end(); ++iter)
+		{
+
+			if (iter->isCrush == true)
+			{
+
+				skillVector.erase(iter);		//이 부분에서 현재 에러
+												//벡터의 원소를 지우면 에러남
+				cout << "현재 " <<(int)iter->skillIndex<<"번째 충돌 , 총 스킬 개수"<< skillVector.size() << endl;
+			}
+
+			if (iter->isCrush == false)
+			{
+				//파이어볼 위치값 갱신
+				if (iter->isRight == true) {
+					iter->skillPos.X += 10;
+				}
+				else {
+					iter->skillPos.X -= 10;
+				}
+				cout << "스킬 [" << iter->skillIndex << "] 위치" << iter->skillPos.X << endl;
+			}
+
+		}
+	}
+	
+}
+void SkillManager::checkCollision(COORD& player1,COORD& player2)
+{
+	float player1MinX = player1.X;
+	float player1MaxX = player1.X + PLAYER_WIDTH;
+	float player1MinY = player1.Y;
+	float player1MaxY = player1.Y + PLAYER_HEIGHT;
+
+
+
+	for (iter = skillVector.begin(); iter != skillVector.end(); ++iter)
+	{
+
+		if (Collision::RRCollision(player1MinX, player1MinY, player1MaxX, player1MaxY, iter->skillPos.X, iter->skillPos.Y,
+			iter->skillPos.X + FIREBALL_WIDTH, iter->skillPos.Y + FIREBALL_HEIGHT)) 
+		{
+			//충돌할경우 isCrush를 true로 만들어라
+			iter->isCrush = true;
+			cout << "충돌" << endl;
+			
+		}
+	}
+}
---
+++ Server/NGP_TeamProject_Server/NGP_TeamProject_Server/SkillManager.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Defines.h"
+#include "Collision.h"
+
+#define PLAYER_WIDTH 75
+#define PLAYER_HEIGHT 70
+#define FIREBALL_WIDTH 173
+#define FIREBALL_HEIGHT 46
+
+class SkillManager
+{
+public:
+	SkillManager();
+	~SkillManager();
+
+	//현재 발동한 스킬 정보 추가
+	void addSkill(byte& player,COORD& skillPos);
+
+	void checkCollision(COORD& player1,COORD& player2);
+	void update(float fTimeElapsed,COORD& player1,COORD& player2);
+private:
+	//map<pair<bool,int>,SC_SKILL> skillMap;
+	vector<SC_SKILL> skillVector;
+
+	vector<SC_SKILL> ::iterator iter;
+};
