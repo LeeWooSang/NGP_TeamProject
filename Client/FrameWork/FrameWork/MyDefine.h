506ce4da57e982d995cf192047866509b09026da
이우상 <lus0506@gmail.com>
2018-12-07 17:52:43 +0900

    no message

--- Client/FrameWork/FrameWork/MyDefine.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "MyInclude.h"
-#define SAFE_DELETE(p) {if(p){ delete(p); (p) = NULL;}}
-#define SAFE_DELETE_ARRAY(p) {if(p){ delete[](p); (p) = NULL;}}
-
-
-
-using std::cout;
